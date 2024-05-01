1ab022c3b6c1156198b4a6ac15e7963f30b94b00
hason123 <hason051004@gmail.com>
2024-05-01 09:10:30 +0700

    collision duoc sua, object bi mat, sound oke, cap nhat vi tri player

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -36,8 +36,8 @@ public class Player extends Entity {
 	}
 
 	public void setDefaultValues() {
-		worldX = gp.tileSize * 23;
-		worldY = gp.tileSize * 21;
+		worldX = gp.tileSize * 5;
+		worldY = gp.tileSize * 5;
 		speed = 4;
 		direction = "down";
 	}
