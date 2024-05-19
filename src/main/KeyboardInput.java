45ef17ac5c68499a027d1b53b7e4fa8f05181aa0
hason123 <hason051004@gmail.com>
2024-05-19 18:04:15 +0700

    Them anh GAME OVER!

--- src/main/KeyboardInput.java
+++ src/main/KeyboardInput.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyListener;
 public class KeyboardInput implements KeyListener {
 
     public boolean diTren, diXuong, diTrai, diPhai, attack,interact;
+
     GamePanel gp;
 
     public KeyboardInput(GamePanel gp) {
@@ -84,97 +85,11 @@ public class KeyboardInput implements KeyListener {
         if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             attack = false;
         }
-
     }
 }
 
-//	public boolean isAnyKeyPressed() {
-//		if(diTren == true || diXuong == true || diTrai == true || diPhai == true) {
-//			anyKeyPressed = true;
-//		}
-//		return anyKeyPressed;
-// Phan o duoi la phan viet lai code
-/*import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
-public class KeyboardInput implements KeyListener {
 
-    public boolean diTren, diXuong, diTrai, diPhai;
-
-    public KeyboardInput() {
-        // Initialize all key states to false
-        diTren = false;
-        diXuong = false;
-        diTrai = false;
-        diPhai = false;
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // Not needed for this implementation
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        // Set the appropriate key state to true when a key is pressed
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_W:
-                diTren = true;
-                break;
-            case KeyEvent.VK_S:
-                diXuong = true;
-                break;
-            case KeyEvent.VK_A:
-                diTrai = true;
-                break;
-            case KeyEvent.VK_D:
-                diPhai = true;
-                break;
-            default:
-                break;
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // Set the appropriate key state to false when a key is released
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_W:
-                diTren = false;
-                break;
-            case KeyEvent.VK_S:
-                diXuong = false;
-                break;
-            case KeyEvent.VK_A:
-                diTrai = false;
-                break;
-            case KeyEvent.VK_D:
-                diPhai = false;
-                break;
-            default:
-                break;
-        }
-    }
-
-    // Getters for the key states
-    public boolean isdiTren() {
-        return diTren;
-    }
-
-    public boolean isdiXuong() {
-        return diXuong;
-    }
-
-    public boolean isdiTrai() {
-        return diTrai;
-    }
-
-    public boolean isdiPhai() {
-        return diPhai;
-    }
-}
-*/
-	
 	
 	
 
